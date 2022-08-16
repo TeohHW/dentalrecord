fb970e3f6c24dfbeb9a3dbdf0a7a21156963cf4a
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:12:52 +0800

    Update RegisterClinic.java

--- src/main/java/com/dentalrecord/servlets/RegisterClinic.java
+++ src/main/java/com/dentalrecord/servlets/RegisterClinic.java
@@ -48,7 +48,7 @@ public class RegisterClinic extends HttpServlet {
 		RequestDispatcher dispatcher = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			PreparedStatement pst = conn.prepareStatement("INSERT INTO clinics(clinicName,clinicAddress,clinicNo) values(?,?,?) ");
 			pst.setString(1, clinicName);
 			pst.setString(2, clinicAddr);
