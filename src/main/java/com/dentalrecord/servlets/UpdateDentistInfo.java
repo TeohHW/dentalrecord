e03c4b7d15a5bededcdfa42cb7827983bd585020
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:20:11 +0800

    Update UpdateDentistInfo.java

--- src/main/java/com/dentalrecord/servlets/UpdateDentistInfo.java
+++ src/main/java/com/dentalrecord/servlets/UpdateDentistInfo.java
@@ -60,7 +60,7 @@ public class UpdateDentistInfo extends HttpServlet {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			clinicId = (int)session.getAttribute("clinicId");
 			pst3 = conn.prepareStatement("SELECT * from users where clinicId = ? and iduserAccounts = ?");
 			pst3.setString(2,userId );
