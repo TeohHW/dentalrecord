e46109dd09b66ea8f6639ecbafa28b2aba33d6e3
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:20:00 +0800

    Update UpdateAssistantInfo.java

--- src/main/java/com/dentalrecord/servlets/UpdateAssistantInfo.java
+++ src/main/java/com/dentalrecord/servlets/UpdateAssistantInfo.java
@@ -58,7 +58,7 @@ public class UpdateAssistantInfo extends HttpServlet {
 		PreparedStatement pst3;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			clinicId = (int)session.getAttribute("clinicId");
 			pst3 = conn.prepareStatement("SELECT * from users where clinicId = ? and iduserAccounts = ?");
 			pst3.setString(2,userId );
