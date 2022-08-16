dfa7ba9e051c9277acac0120481a7dfc31a257c0
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:13:36 +0800

    Update RegisterDentist.java

--- src/main/java/com/dentalrecord/servlets/RegisterDentist.java
+++ src/main/java/com/dentalrecord/servlets/RegisterDentist.java
@@ -55,7 +55,7 @@ public class RegisterDentist extends HttpServlet {
 		int clinicId;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			clinicId = (int)session.getAttribute("clinicId");
 			
 			PreparedStatement pst3 = conn.prepareStatement("SELECT * from clinics where clinicId = ?");
