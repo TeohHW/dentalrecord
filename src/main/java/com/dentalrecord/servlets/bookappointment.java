653f27af709918e18b36f16141c0867fe5c95f93
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:21:58 +0800

    Update bookappointment.java

--- src/main/java/com/dentalrecord/servlets/bookappointment.java
+++ src/main/java/com/dentalrecord/servlets/bookappointment.java
@@ -52,7 +52,7 @@ public class bookappointment extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			PreparedStatement pst = conn.prepareStatement("INSERT INTO appointment (custid, docname, services, date, apptime, clinicId) values (?,?,?,?,?,?)");
 			pst.setString(1, id);
 			pst.setString(2, docname);
