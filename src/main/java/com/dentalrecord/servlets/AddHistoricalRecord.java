5dbf17df1c9b9b3e39b59801c18230ec2869b31a
Teoh Hong Wei <teohhongwei@hotmail.com>
2022-08-16 20:08:23 +0800

    Update AddHistoricalRecord.java

--- src/main/java/com/dentalrecord/servlets/AddHistoricalRecord.java
+++ src/main/java/com/dentalrecord/servlets/AddHistoricalRecord.java
@@ -51,7 +51,7 @@ public class AddHistoricalRecord extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dentalrecord?useSSL=false","root","passwordroot");
+			conn = DriverManager.getConnection("jdbc:mysql://be411b9b7c9b30:cf5f4f3e@us-cdbr-east-06.cleardb.net/heroku_b9c43cd0bc4ec95?reconnect=true");
 			PreparedStatement pst = conn.prepareStatement("insert into historicalrecords(history, historyDate, description, idcustomer) values(?,?,?,?)");
 			pst.setString(1, history);
 			pst.setString(2, historyDate);
